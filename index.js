0d6b3a5415863bf56d873eeee4c78a6b60a50475
Gytis Trikleris <gytis@redhat.com>
2020-04-28 20:01:24 +0200

    changes

--- index.js
+++ index.js
@@ -14,17 +14,13 @@ async function setupGit() {
     core.info("Starting git setup");
     await exec.exec(`git config --local user.email "gytis@redhat.com"`);
     await exec.exec(`git config --local user.name "${github.context.actor}"`);
-    await exec.exec(`git remote set-url origin https://${context.context.action}:${params.token}@github.com/${context.context.repo.owner}/${context.context.repo.repo}.git`);
-    await exec.exec('git remote show origin');
     await exec.exec(`git checkout ${params.branch}`);
-    // TODO might need to setup a token
     core.info("Completed git setup");
 }
 
 async function prepareRelease() {
     core.info("Starting release preparation");
-    // let command = `mvn release:prepare -B -Dusername=${github.context.actor} -Dpassword=${params.token} -DpushChanges=false`;
-    let command = `mvn release:prepare -B -DpushChanges=false`;
+    let command = `mvn release:prepare -B -Dusername=${github.context.actor} -Dpassword=${params.token}`;
     command += params.releaseVersion ? ` -DreleaseVersion=${params.releaseVersion}` : '';
     command += params.developmentVersion ? ` -DdevelopmentVersion=${params.developmentVersion}` : '';
     command += params.tag ? ` -DreleaseVersion=${params.tag}` : '';
@@ -41,4 +37,4 @@ async function performRelease() {
 setupGit()
     .then(() => prepareRelease())
     .then(() => performRelease())
-    .catch(reason => core.setFailed(error.message));
+    .catch(reason => core.setFailed(reason));
