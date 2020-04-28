7a54a2d23a88707dddc64c7619376a6b7e3b3853
gytis <gytis@redhat.com>
2020-04-28 20:51:57 +0200

    extract params

--- index.js
+++ index.js
@@ -2,31 +2,33 @@ const github = require('@actions/github');
 const core = require('@actions/core');
 const exec = require('@actions/exec');
 
-const params = {
-    token: core.getInput('token'),
-    branch: core.getInput('branch'),
-    releaseVersion: core.getInput('releaseVersion'),
-    developmentVersion: core.getInput('developmentVersion'),
-    tag: core.getInput('tag')
+function initParams() {
+    return {
+        token: core.getInput('token'),
+        branch: core.getInput('branch'),
+        releaseVersion: core.getInput('releaseVersion'),
+        developmentVersion: core.getInput('developmentVersion'),
+        tag: core.getInput('tag'),
+        user: github.context.actor,
+        email: 'gytis@redhat.com',
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo
+    }
 }
 
-async function setupGit() {
+async function setupGit(params) {
     core.info("Starting git setup");
-    await exec.exec(`git config --local user.email "gytis@redhat.com"`);
-    await exec.exec(`git config --local user.name "gytis"`);
-    core.info(github.context.actor);
-    core.info(params.token);
-    core.info(github.context.repo.owner);
-    core.info(github.context.repo.repo);
-    await exec.exec(`git remote set-url origin https://${github.context.actor}:${params.token}@github.com/${github.context.repo.owner}/${github.context.repo.repo}.git`);
-    await exec.exec(`git checkout ${params.branch}`);
+    await exec.exec(`git config --local user.email "${params.email}"`);
+    await exec.exec(`git config --local user.name "${params.user}"`);
+    exec.exec(`git remote set-url origin https://${params.user}:${params.token}@github.com/${params.owner}/${params.repo}.git`);
+    exec.exec(`git checkout ${params.branch}`);
     // TODO might need to setup a token
     core.info("Completed git setup");
 }
 
-async function prepareRelease() {
+async function prepareRelease(params) {
     core.info("Starting release preparation");
-    let command = `mvn release:prepare -B -Dusername=${github.context.actor} -Dpassword=${params.token}`;
+    let command = `mvn release:prepare -B`;
     command += params.releaseVersion ? ` -DreleaseVersion=${params.releaseVersion}` : '';
     command += params.developmentVersion ? ` -DdevelopmentVersion=${params.developmentVersion}` : '';
     command += params.tag ? ` -DreleaseVersion=${params.tag}` : '';
@@ -40,7 +42,12 @@ async function performRelease() {
     core.info("Released");
 }
 
-setupGit()
-    .then(() => prepareRelease())
-    .then(() => performRelease())
-    .catch(reason => core.setFailed(reason));
+try {
+    const params = initParams();
+    setupGit(params)
+        .then(() => prepareRelease(params))
+        .then(() => performRelease())
+        .catch(reason => core.setFailed(reason));
+} catch (e) {
+    core.setFailed(e.message);
+}
